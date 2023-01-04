857b1490603f50f8597e2590b49328ac32d78a49
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-01-04 08:49:27 -0300

    create utilities icons

---
+++ src/utilities/Icons.ts
@@ -0,0 +1,8 @@
+export const iconsLib = {
+  'Sun-cloud-mid-rain': require('../../assets/icons/Sun-cloud-mid-rain.png'),
+  'Cloud-zap': require('../../assets/icons/Cloud-zap.png'),
+  'Moon-cloud-fast-wind': require('../../assets/icons/Moon-cloud-fast-wind.png'),
+  'Mid-snow-fast-winds': require('../../assets/icons/Mid-snow-fast-winds.png'),
+  'Moon-fast-wind': require('../../assets/icons/Moon-fast-wind.png'),
+  'Position-3D': require('../../assets/icons/Position-3D.png'),
+};
---
+++ src/utilities/index.ts
@@ -0,0 +1,1 @@
+export {iconsLib} from './Icons';
