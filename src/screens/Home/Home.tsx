b4aae6e69577b08988dbcaa5ace32b2c8a1ce2d6
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-01-04 08:53:02 -0300

    refactoring screen home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Container, BallonGroup, MiniCardGroup} from './styles';
+import {Container, BallonGroup, MiniCardGroup, CardDayGroup} from './styles';
 
 import {BalloonClimate} from '../../components/BalloonClimate';
 import {MiniCardClimate} from '../../components/MiniCardClimate/MiniCardClimate';
 
 import {Card} from '../../components/Card';
+import {CardDay} from '../../components/CardDay';
+import {TextInput} from '../../components/Input';
 
 export function Home() {
+  const [address, setAddress] = useState('');
   return (
     <Container>
-      <Card climate="Cloud-zap" />
+      <TextInput.Root>
+        <TextInput.Icon />
+        <TextInput.Field
+          placeholder="Estrada do Regalado"
+          value={address}
+          onChangeText={setAddress}
+        />
+      </TextInput.Root>
+
+      <Card
+        temperature="28°"
+        feel="Feels like 22°"
+        timeCourse="Heavy Rain"
+        timeDay="Tonight"
+        climate="Cloud-zap"
+      />
 
       <MiniCardGroup>
-        <MiniCardClimate opacity type="rain" value={28} />
+        <MiniCardClimate type="rain" value={28} />
         <MiniCardClimate type="wind" value={8} />
         <MiniCardClimate type="humidity" value={64} />
       </MiniCardGroup>
@@ -31,6 +49,26 @@ export function Home() {
           info={{hour: '13:00', temperature: '29°'}}
         />
       </BallonGroup>
+
+      <CardDayGroup>
+        <CardDay
+          dayNumber={29}
+          month="Sep"
+          weekDay="Monday"
+          temperature="17°/22°"
+          iconClimate="Cloud-zap"
+          climate="Cloudy"
+        />
+
+        <CardDay
+          dayNumber={30}
+          month="Sep"
+          weekDay="Tuesday"
+          temperature="22°/27°"
+          iconClimate="Moon-fast-wind"
+          climate="Storm"
+        />
+      </CardDayGroup>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,23 +7,34 @@ export const Container = styled(View)`
   align-items: center;
 
   height: 100%;
+  padding: 0 50px;
 `;
 
 export const BallonGroup = styled(View)`
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
 
-  width: 90%;
+  width: 100%;
   margin-bottom: 20px;
 `;
 
 export const MiniCardGroup = styled(View)`
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
 
-  width: 90%;
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const CardDayGroup = styled(View)`
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
   margin-top: 20px;
   margin-bottom: 20px;
 `;
