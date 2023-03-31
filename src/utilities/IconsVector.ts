0ccaf294452f6919449e43f4f9c3f77139286de6
guilhermehilario <guilhermehilariosilva@gmail.com>
2023-03-31 18:46:19 -0300

    add icon arrow-left svg

--- src/utilities/IconsVector.ts
+++ src/utilities/IconsVector.ts
@@ -1,7 +1,9 @@
 import Bars from '../../assets/icons/Bars.svg';
 import Compress from '../../assets/icons/compress.svg';
+import ArrowLeft from '../../assets/icons/Arrow-left.svg';
 
 export const iconsVector = {
   bars: Bars,
   compress: Compress,
+  arrowLeft: ArrowLeft,
 };
